93035baa0f3662b8ef2fb366bf3acc0bad95fc44
Deepa-Universe <deepa.miracles@gmail.com>
2020-01-16 15:16:28 +0530

    new changes in js file

--- main3.js
+++ main3.js
@@ -1,66 +1,44 @@
 //let
-let intValue = 10;
 
-//Exception throw
-//console.log(intValue6); //intValue6 is not defined
-//Curly braces indicates scope
+
+let intValue4 = 200;
+
+var globalIntVariable = "Deepa";
+//Exception throw 
+//console.log(intValue2); //intValue2 is not defined
+//var intValue3 = "Deepa"; //Out of Scope we can have variable with same name, but best practice would be to keep variables with different names
 {
-    let intValue67 = 1991;
-
+    let intValue3 = 100;
     //Exception throw
-    //console.log(intValue5); //intValue5 is not defined
-    console.log(intValue); //If 'let' variable is defined outside, it is accessible inside the scope
-}
+    //console.log(intValue1); //intValue1 is not defined
 
-{
-    //Throws exception
-    //intValue = 15; //Identifier 'intValue' has already been declared
-    console.log(intValue);
-    //console.log(intValue6);
-    intValue67 = 1992;
 
-    let intNewValue = 1993;
-}
+    globalIntVariable = globalIntVariable + " Cafe";
 
-console.log(intValue67);
-console.log(intValue);
-console.log(intNewValue);
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/*
-//var
-var intValue2 = 100;
-console.log(intValue7); //Undefined
-//Curly braces indicates scope
-{
-    var intValue7 = 1206;
-    console.log(intValue2); //If 'var' variable is defined outside, it is accessible inside the scope
-}
-
-{
-    intValue2 = 150;
-    console.log(intValue2);
-}
-
-console.log(intValue2); 
-console.log(intValue7); //Var is accessible through out the page
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-//const
-/*const intValue3 = 1000;
-
-//Curly braces indicates scope
-{
-    console.log(intValue3); //If 'const' variable is defined outside, it is accessible inside the scope
-}
-
-{
-    //const intValue3 = 1500; //1500
-
-    //Exception
-    //intValue3 = 56; //Assignment to constant variable
     console.log(intValue3);
+    console.log(intValue4); //Variable declared outside the scope is global and is accessible inside the scope
+
+    console.log(globalIntVariable);
 }
 
-console.log(intValue3); */
+//console.log(intValue3); //intValue3 is not defined-- Variable is inaccessible out of the scope.
+console.log(intValue4);
+
+//console.log(intValue3);
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//var
+
+console.log(intValue5); //undefined
+var intValue5;
+console.log(intValue5); //undefined
+{
+    console.log(intValue5); //undefined
+    var intValue6 = 600;
+
+    globalIntVariable = "Deepa- Your Birthday"; //var is always global
+}
+console.log(intValue6); //var is accessible outside the scope
+
+console.log(globalIntVariable);
